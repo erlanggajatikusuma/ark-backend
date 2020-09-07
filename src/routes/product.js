58646b1812a81e2ce23c1a77dc9a8a7264ca130a
erlanggajatikusuma <erlanggajatikusuma007@gmail.com>
2020-09-07 20:23:24 +0700

    remove auth getproduct & byId

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,17 +1,15 @@
 const express = require('express')
 const router = express.Router()
-// connect controller
 const controller = require('../controller/product')
 const auth = require('../middlewares/auth')
 const multer = require('../middlewares/multer')
 const pagination = require('../middlewares/pagination')
-// const {cache, clearCache, cacheProductId} = require('../middlewares/redis')
 
 
 // route endpoints
 router
-  .get('/', auth.verifyAccess, pagination.products, controller.getAllProduct)
-  .get('/:id',auth.verifyAccess, controller.getProductById)
+  .get('/', pagination.products, controller.getAllProduct)
+  .get('/:id', controller.getProductById)
   .post('/',auth.verifyAccess, auth.isAdmin, multer.upload.single('image'), controller.insertNewProduct)
   .patch('/:id',auth.verifyAccess, auth.isAdmin, multer.upload.single('image'), controller.updateProduct)
   .delete('/:id',auth.verifyAccess, auth.isAdmin, controller.deleteProduct)
